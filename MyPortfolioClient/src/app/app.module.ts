ec2954864c75658c3888821ec22a7ff9c35ebb4d
Sreenath Angamuthu <sreenath096@gmail.com>
2022-09-25 15:36:22 +0530

    Add home and nav component

--- MyPortfolioClient/src/app/app.module.ts
+++ MyPortfolioClient/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
